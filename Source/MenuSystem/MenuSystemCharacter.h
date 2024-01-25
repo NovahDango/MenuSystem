6110b055f61866f6473c9ad0078b7b2c45a90666
novahdango <noahmachado02@gmail.com>
2024-01-25 16:18:09 -0500

    Added functionality for creating sessions. Modified session settings. Configured DefaultEngine.ini to support sessions.

--- Source/MenuSystem/MenuSystemCharacter.h
+++ Source/MenuSystem/MenuSystemCharacter.h
@@ -4,6 +4,7 @@
 
 #include "CoreMinimal.h"
 #include "GameFramework/Character.h"
+#include "Interfaces/OnlineSessionInterface.h"
 #include "Logging/LogMacros.h"
 #include "MenuSystemCharacter.generated.h"
 
@@ -73,6 +74,15 @@ public:
 public:
 
 	// Pointer to the online session interface
-	TSharedPtr<class IOnlineSession, ESPMode::ThreadSafe> OnlineSessionInterface;
+	IOnlineSessionPtr OnlineSessionInterface;
+
+protected:
+	UFUNCTION(BlueprintCallable)
+	void CreateGameSession();
+
+	void OnCreateSessionComplete(FName SessionName, bool bWasSuccessful);
+
+private: 
+	FOnCreateSessionCompleteDelegate CreateSessionCompleteDelegate;
 };
 
